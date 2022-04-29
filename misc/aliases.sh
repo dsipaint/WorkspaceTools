488a71cde2a72a6f29addc52de09f3933863bf46
dsipaint <finlay.woolfson@gmail.com>
2022-04-29 10:53:01 +0100

    Add twitch CLI

---
+++ misc/aliases.sh
@@ -0,0 +1,1 @@
+twitch=~/twitch-cli_1.1.6_Linux_x86_64/twitch
--- setup.sh
+++ setup.sh
@@ -2,4 +2,8 @@
 
 #setup the scripts here
 
+#install twitch CLI
+wget https://github.com/twitchdev/twitch-cli/releases/download/v1.1.6/twitch-cli_1.1.6_Linux_x86_64.tar.gz -P ~
+tar -xf ~/twitch-cli_1.1.6_Linux_x86_64.tar.gz
+
 echo Al\'s workspace tools are set up for use- happy coding! 
