4dbaa50ec057de24c1c944c559b2c2021d8119ab
Ubuntu <ubuntu@ip-172-31-37-215.eu-west-1.compute.internal>
2022-05-19 12:06:33 +0000

    Fix sed command

--- misc/aws_ssh_pass.sh
+++ misc/aws_ssh_pass.sh
@@ -3,7 +3,7 @@
 # this script will allow password authentication on an AWS EC2 instance, in case you either want
 # to use a password or want to use the VSC ssh editor on the EC2 instance
 
-sudo sed -i 's/PasswordAuthentication: no/PasswordAuthentication: yes' /etc/ssh/sshd_config
+sudo sed -i 's/PasswordAuthentication: no/PasswordAuthentication: yes/g' /etc/ssh/sshd_config
 sudo passwd ubuntu
 sudo service sshd restart
 echo You can now login as ubuntu using the password you set on this machine
