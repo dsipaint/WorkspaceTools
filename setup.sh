2da88537361bcba16c875f76f9ede5250e888cbf
dsipaint <finlay.woolfson@gmail.com>
2022-08-11 19:44:24 +0100

    Add some... upgrades

--- setup.sh
+++ setup.sh
@@ -6,8 +6,19 @@ cd
 #load scripts
 source ~/WorkspaceTools/java/AMGN.sh
 source ~/WorkspaceTools/java/jdk17.sh
+
 source ~/WorkspaceTools/misc/aliases.sh
 source ~/WorkspaceTools/misc/aws.sh
 source ~/WorkspaceTools/misc/install.sh
 source ~/WorkspaceTools/misc/tools.sh
 source ~/WorkspaceTools/misc/uninstall.sh
+
+
+#add cool ascii art B)
+RED='\033[0;31m'
+echo -e "${RED}.__          __ /\        .__                   __   ._."
+echo "|  |   _____/  |)/ ______ |  |__ _____    ____ |  | _| |"
+echo "|  | _/ __ \   __\/  ___/ |  |  \\__  \ _/ ___\|  |/ / |"
+echo "|  |_\  ___/|  |  \___ \  |   Y  \/ __ \\  \___|    < \|"
+echo "|____/\___  >__| /____  > |___|  (____  /\___  >__|_ \__"
+echo "          \/          \/       \/     \/     \/     \/\/"
