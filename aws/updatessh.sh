ffe4606d5a4e1ba4165a0cc6188fa72b0ca3c436
al-desktop <finlay.woolfson@gmail.com>
2024-09-18 01:56:57 +0100

    ssh config auto updates

--- aws/updatessh.sh
+++ aws/updatessh.sh
@@ -8,4 +8,5 @@ do
     box_ip=$(aws ec2 describe-instances --filters 'Name=tag:Name, Values=testbox' 'Name=instance-state-name, Values=pending,running,stopping,stopped' | jq -r .Reservations[0].Instances[0].PublicIpAddress)
 done
 
-sed -i -z "s/Host testbox\n  HostName.*\n  User ubuntu/Host testbox\n  HostName ${box_ip}\n  User ubuntu/g" /mnt/c/Users/finla/.ssh/config
+old_ip=$(grep -A 3 --color=never "Host testbox" /mnt/c/users/finla/.ssh/config | grep --color=never -P -o '(\d{1,3}\.){3}\d{1,3}')
+sed -i "s/${old_ip}/${box_ip}/g" /mnt/c/Users/finla/.ssh/config
